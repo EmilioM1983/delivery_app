eb0b37944852ce762852f485e26e7fa0c7880e91
EmilioM1983 <contact.ehmsoft@gmail.com>
2025-06-09 20:10:50 -0300

    Preparamos para hacer la integracion 3

--- src/main/java/org/IngSoft/Main.java
+++ src/main/java/org/IngSoft/Main.java
@@ -1,10 +1,5 @@
 package org.IngSoft;
 
-import org.IngSoft.service.DeliveryTrackingService;
-import org.IngSoft.ui.RastreoUi;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
