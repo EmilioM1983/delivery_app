5733d189fd1f2356caeaee9c07ab76f537f14334
EmilioM1983 <contact.ehmsoft@gmail.com>
2025-06-09 20:11:34 -0300

    Preparamos para hacer la integracion 3

--- src/main/java/org/IngSoft/Main.java
+++ src/main/java/org/IngSoft/Main.java
@@ -1,11 +1,5 @@
 package org.IngSoft;
 
-import org.IngSoft.database.Conexion;
-import org.IngSoft.ui.MenuAppUi;
-import org.IngSoft.ui.PaymentUI;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
