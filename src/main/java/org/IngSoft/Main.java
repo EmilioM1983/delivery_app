e8c2eb74906e12c9b1ab017d7c7107ea39bc8da3
EmilioM1983 <contact.ehmsoft@gmail.com>
2025-06-09 20:09:53 -0300

    Preparamos para hacer la integracion 2

--- src/main/java/org/IngSoft/Main.java
+++ src/main/java/org/IngSoft/Main.java
@@ -1,10 +1,5 @@
 package org.IngSoft;
-import org.IngSoft.ui.OrderUi;
 
-import org.IngSoft.database.Conexion;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
