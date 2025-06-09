f5fcb508d7a891cf46c7a196a0663c8bc0e2f5b2
EmilioM1983 <contact.ehmsoft@gmail.com>
2025-06-09 20:09:03 -0300

    Preparamos para hacer la integracion

--- src/main/java/org/IngSoft/Main.java
+++ src/main/java/org/IngSoft/Main.java
@@ -1,18 +1,7 @@
 package org.IngSoft;
 
-import org.IngSoft.ui.ProductUi;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Crear la instancia de ProductUi
-        ProductUi productUi = new ProductUi();
 
-
-
-
-        // Mostrar el menú de la aplicación para gestionar productos
-        productUi.mostrarMenu();
     }
 }
